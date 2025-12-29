dc862d0e0cc92377ae7b56ce45d20f80dfb249d2
Rania <raniaalasadi8@gmail.com>
2025-12-29 20:30:02 +0300

    1st and 2nd test case

--- cypress/e2e/1-getting-started/Task2.cy.js
+++ cypress/e2e/1-getting-started/Task2.cy.js
@@ -1,5 +1,21 @@
 /// <reference types="cypress" />
-describe('Task2', () => {
-  it('it title', () => {
+describe('Create New User Account', () => {
+  it('Validate that a new user can successfully register using the "Signup / Login" page.', () => {
+    cy.visit("https://automationexercise.com/login")
+    cy.get("[data-qa=signup-name]").type("Rania")
+    cy.get("[data-qa=signup-email]").type("rania502016@hotmail.com")
+    cy.get("[data-qa=signup-button]").click()
+    cy.url().should("eq","https://automationexercise.com/signup")
+  })
+})
+/// <reference types="cypress" />
+describe('User Login​', () => {
+  it('Validate that an existing user can log in using valid credentials', () => {
+    cy.visit("https://automationexercise.com/login")
+    cy.get("[data-qa=login-email]").type("rania502016@hotmail.com")
+    cy.get("[data-qa=login-password]").type("Rania$12345")
+    cy.get("[data-qa=login-button]").click()
+    cy.url().should("eq","https://automationexercise.com/")
+    cy.get(".fa-lock").should("be.visible")
   })
 })
