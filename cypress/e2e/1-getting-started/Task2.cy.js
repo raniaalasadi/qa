d50201aa42bcefe5ae0c79a578070ae9ad153a85
Rania <raniaalasadi8@gmail.com>
2026-01-02 22:00:34 +0300

    done

--- cypress/e2e/1-getting-started/Task2.cy.js
+++ cypress/e2e/1-getting-started/Task2.cy.js
@@ -52,3 +52,22 @@ describe('Add Product to Cart​', () => {
     cy.get(".cart_quantity").should("contain.text","1")
   })
 })
+
+/// <reference types="cypress" />
+describe('Add a Product Review​​', () => {
+  it('Validate that a logged-in user can add a review to a product​', () => {
+    cy.visit("https://automationexercise.com/login")
+    cy.get("[data-qa=login-email]").type("rania502016@hotmail.com")
+    cy.get("[data-qa=login-password]").type("Rania$12345")
+    cy.get("[data-qa=login-button]").click()
+    cy.url().should("eq", "https://automationexercise.com/")
+    cy.get(".navbar-nav").contains(" Logged in as").should("be.visible")
+    cy.get(".choose a").first().click()
+    cy.url().should("eq", "https://automationexercise.com/product_details/1")
+    cy.get("#name").type("Rania")
+    cy.get("#email").type("rania502016@hotmail.com")
+    cy.get("#review").first().type("good price")
+    cy.get("#button-review").click()
+    cy.get(".alert-success span").should("be.visible")
+  })
+})
